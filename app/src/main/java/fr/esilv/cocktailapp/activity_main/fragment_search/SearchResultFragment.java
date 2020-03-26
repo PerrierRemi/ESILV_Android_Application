f3263b8db083420a95ad81ccb75424da2ea2d2db
Remi <dev.perrier.remi@gmail.com>
2020-03-26 13:54:04 +0100

    Fix imports and change home icons

--- app/src/main/java/fr/esilv/cocktailapp/activity_main/fragment_search/SearchResultFragment.java
+++ app/src/main/java/fr/esilv/cocktailapp/activity_main/fragment_search/SearchResultFragment.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -15,7 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 import fr.esilv.cocktailapp.R;
-import fr.esilv.cocktailapp.activity_category.CocktailAdapter;
+import fr.esilv.cocktailapp.adapter.CocktailAdapter;
 import fr.esilv.cocktailapp.api.Cocktail;
 import fr.esilv.cocktailapp.api.CocktailArray;
 import fr.esilv.cocktailapp.api.TheCocktailDBService;
